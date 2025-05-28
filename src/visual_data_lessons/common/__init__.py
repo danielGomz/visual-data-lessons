5c9eecb1082e1545bad854f738aa83a8630edf0d
Daniel Gomez <13943878-danielGomz@users.noreply.gitlab.com>
2025-05-27 18:57:39 -0600

    feat: add OutroScene to the package and export it in __all__

--- src/visual_data_lessons/common/__init__.py
+++ src/visual_data_lessons/common/__init__.py
@@ -1,9 +1,11 @@
-from .scenes.intro import IntroScene
-from .scenes.base_scene import BaseScene
 from .objects.subscribe_reminder import SubscribeReminder
+from .scenes.base_scene import BaseScene
+from .scenes.intro import IntroScene
+from .scenes.outro import OutroScene
 
 __all__ = [
     "IntroScene",
     "BaseScene",
     "SubscribeReminder",
+    "OutroScene",
 ]
